c87fd8fde1ae8fe0d0af03a583144e320fec4e9c
Chalemur <161484705+Chalemur@users.noreply.github.com>
2024-03-04 20:41:19 +0000

    Review Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,21 +8,6 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
-        // Properties
-        //public Die die1 = new();
-        //public Die die2 = new();
-        //public Die die3 = new();
-
-
-        //Methods
-        //public int Sum()
-        //{
-            //int sum = die1.Roll() + die2.Roll() + die3.Roll();
-            //Console.WriteLine(sum);
-            //return sum;
-        //}
-
-
         //Properties
         private Die _die1 = new Die();
         private Die _die2 = new Die();
