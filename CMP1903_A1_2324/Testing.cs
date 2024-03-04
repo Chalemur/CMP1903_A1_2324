4b499f136209d647660137eb06ca2df83ab0c6f2
Chalemur <161484705+Chalemur@users.noreply.github.com>
2024-03-04 20:40:19 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -9,19 +9,19 @@ namespace CMP1903_A1_2324
     internal class Testing
     {
         // Properties
-        private Game game = new();
-
-
+        private Game _game = new Game();  // Creates game object
+        
+        
         //Methods
         public void Main()
         {
-           int sum = game.Sum();
-           int die1 = game.die1.value;
-           int die2 = game.die2.value;
-           int die3 = game.die3.value;
-           Debug.Assert(die1 >= 1 && die1 <= 6 && die2 >= 1 && die2 <= 6 && die3 >= 1 && die3 <= 6, "Value for dice is not valid");
-           Debug.Assert(sum >= 3 && sum <= 18, "Sum not valid");
-
+           int sum = _game.Sum();  //Runs game method and gets a value for the sum of the three dice rolls
+           int die1 = _game.die1Roll;
+           int die2 = _game.die2Roll;
+           int die3 = _game.die3Roll;  //assigns dice roll value from game object to a variable for comparison
+           Debug.Assert(die1 >= 1 && die1 <= 6 && die2 >= 1 && die2 <= 6 && die3 >= 1 && die3 <= 6, "Value for dice is not valid");  //Checks if all dice rolls are between 1-6
+           Debug.Assert(sum >= 3 && sum <= 18, "Sum not valid");  //Checks if sum is valid by making sure its within a certain range of accepted values
+        
         }
     }
 }
